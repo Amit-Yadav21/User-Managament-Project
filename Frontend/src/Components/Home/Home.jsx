6a37ad96fa49cbd2473a1d0f53b5671148cde941
Amit-Yadav21 <amit21navgurukul.org>
2024-10-21 23:02:02 +0530

    Design home page

--- Frontend/src/Components/Home/Home.jsx
+++ Frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Home.css'; 
+import './Home.css';
 
 const Home = () => {
   return (
@@ -7,19 +7,19 @@ const Home = () => {
       <div className='border'>
         <h1 className="cdd-text">User Management System ?</h1>
         <p>
-          What is a User Management System?
-          A User Management System is a software solution designed to manage user accounts and their access to resources within an application or system.</p>
-          <ul>
-            <li>1. User Registration and Login: Allow users to create accounts and authenticate themselves to access the system.</li>
-            <li>2. Profile Management: Enable users to view and update their personal information, such as name, email, password, and other relevant data.</li>
-            <li>3. Role-Based Access Control (RBAC): Assign different roles to users (e.g., admin, editor, viewer) and manage permissions based on these roles, ensuring that users have appropriate access to resources.</li>
-            <li>4. Password Management: Include features for password recovery, reset, and enforcement of password policies (e.g., complexity requirements).</li>
-            <li>5. Account Security: Implement measures like two-factor authentication (2FA) and activity logging to enhance security.</li>
-            <li>6. User Activity Tracking: Monitor user actions within the system for audit and analysis purposes.</li>
-            <li>7. User Deactivation and Deletion: Allow for the deactivation or permanent deletion of user accounts when necessary.</li>
-          </ul>
+          A User Management System is a software solution designed to manage user accounts and their access to resources within an application or system.
+        </p>
+        <p>✦<b>The primary functions of such a system include</b>✦</p>
+        <ul>
+          <li>➢ User Registration and Login: Allow users to create accounts and authenticate themselves to access the system.</li>
+          <li>➢ Profile Management: Enable users to view and update their personal information, such as name, email, password, and other relevant data.</li>
+          <li>➢ Role-Based Access Control (RBAC): Assign different roles to users (e.g., admin, editor, viewer) and manage permissions based on these roles, ensuring that users have appropriate access to resources.</li>
+          <li>➢ Password Management: Include features for password recovery, reset, and enforcement of password policies (e.g., complexity requirements).</li>
+          <li>➢ Account Security: Implement measures like two-factor authentication (2FA) and activity logging to enhance security.</li>
+          <li>➢ User Activity Tracking: Monitor user actions within the system for audit and analysis purposes.</li>
+          <li>➢ User Deactivation and Deletion: Allow for the deactivation or permanent deletion of user accounts when necessary.</li>
+        </ul>
       </div>
-      {/* <button className='btn'>View</button> */}
     </div>
   );
 };
